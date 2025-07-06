6ba7ff128f34bfcd017f91460e5ba738e4481b5a
Fatemeh Ebrahimi <fatemeh.ebrahimi.russophile@gmail.com>
2025-07-06 17:25:22 +0330

    Add files via upload

---
+++ KNN/Cat-or-Dog/load-cat-dog-model.py
@@ -0,0 +1,29 @@
+import cv2 as cv
+from glob import glob
+from joblib import load
+import numpy as np
+
+model = load(r'C:\Users\pc\Documents\university file\Machine Learning\Machine learning files\part3\f.ebrahimi.Q1\cat-dog-model.joblib')
+
+data = []
+label = []
+
+for address in glob(r'C:\Users\pc\Documents\university file\Machine Learning\Machine learning files\part3\f.ebrahimi.Q1\load_dataset\*'):
+
+    img = cv.imread(address)
+    img_r = cv.resize(img,(32,32))
+    img_r = img_r / 255.0
+    img_r = img_r.flatten()
+    data.append(img_r)
+
+    img_r = np.array([img_r])
+    pre = model.predict(img_r)[0]
+
+    x = address.split('\\')[-1].split('.')[0]
+    label.append(x)
+
+    cv.putText(img,str(pre),(250,250),cv.FONT_HERSHEY_SCRIPT_COMPLEX,2,(230,216,173),5)
+    cv.imshow("image",img)
+    cv.waitKey(0)
+
+print(f'the accurancy score is :{model.score(data,label)*100}%')
