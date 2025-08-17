50ee9e9aef5b579cf4725c3cd3e7b0dbdc870ca4
Fatemeh Ebrahimi <fatemeh.ebrahimi.russophile@gmail.com>
2025-08-17 14:27:52 +0330

    Update train.py

--- Linear Regression/MultiVariableRegression/train.py
+++ Linear Regression/MultiVariableRegression/train.py
@@ -1,6 +1,8 @@
 import numpy as np
 import pandas as pd
 from sklearn.preprocessing import StandardScaler
+import matplotlib.pyplot as plt
+from joblib import dump
 
 # Load dataset
 df = pd.read_csv(r'C:\Users\pc\Documents\university file\Machine Learning\Machine learning files\part5\F.Ebrahimi.boston\BostonHousing.csv')
@@ -10,8 +12,10 @@ train = np.array(df)
 train_x = train[:, :-1]
 train_y = train[:, -1]
 
-sk = StandardScaler()
-train_x = sk.fit_transform(train_x)
+# Normalize and save the file for testS
+scaler = StandardScaler()
+train_x = scaler.fit_transform(train_x)
+dump(scaler,r'C:\Users\pc\Documents\university file\Machine Learning\Machine learning files\part5\F.Ebrahimi.boston\scaler.pkl')
 
 # Add Bias term
 m = train.shape[0]
@@ -60,3 +64,6 @@ for epoch in range(100):
     grads = gradients(train_x,train_y,y_hat)
     w = gradient_decent(w,0.1,grads)
     print(f'the epoch : {epoch}\nits error : {e}\nits weights : {w}\n{stars}\n')
+
+# Save the weights for test files
+np.save(r'C:\Users\pc\Documents\university file\Machine Learning\Machine learning files\part5\F.Ebrahimi.boston\MultiVariablelinearRegressionWeights.csv',w)
